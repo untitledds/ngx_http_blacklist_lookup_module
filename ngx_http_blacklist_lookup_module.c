dd7d955d1066e5278389d38fc7b38096e0ba83ea
Aleksey <untitledds@users.noreply.github.com>
2024-11-07 15:22:45 +0300

    Update ngx_http_blacklist_lookup_module.c

--- ngx_http_blacklist_lookup_module.c
+++ ngx_http_blacklist_lookup_module.c
@@ -182,7 +182,7 @@ static int lookupAddr(ngx_str_t *ip_as_string, ngx_str_t *ipstr) {
 
     // Преобразуем in_addr_t в строку
     inaddr.s_addr = addr;
-    if (ngx_inet_ntop(AF_INET, &inaddr, ipstr->data, buffer_size) == NULL) {
+    if (ngx_inet_ntop(AF_INET, &inaddr, ipstr->data, buffer_size) == NGX_ERROR) {
         if (ngx_http_blacklist_lookup_verbose) {
             ngx_log_error(NGX_LOG_ERR, ngx_cycle->log, 0, "Failed to convert IP address to string: %V", ip_as_string);
         }
